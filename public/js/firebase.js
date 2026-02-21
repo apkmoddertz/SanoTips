b3a6915320c46205b8cb1335fdd91e225a8b1837
apkmoddertz <102845049+apkmoddertz@users.noreply.github.com>
2026-02-21 15:22:17 +0300

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,5 @@
-// public/js/firebase.js
+// js/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
@@ -6,21 +7,26 @@ import { getFirestore, collection, getDocs } from "https://www.gstatic.com/fireb
 const firebaseConfig = {
   apiKey: "AIzaSyC0BSUvubGtHxeHZ_pnh776bzoGKGxXNbU",
   authDomain: "voltflow-35a0b.firebaseapp.com",
-  databaseURL: "https://voltflow-35a0b-default-rtdb.firebaseio.com",
   projectId: "voltflow-35a0b",
   storageBucket: "voltflow-35a0b.firebasestorage.app",
   messagingSenderId: "241701625823",
   appId: "1:241701625823:web:d4d7d9362e0207e5a32e6a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Fetch all matches from Firestore "matches" collection
+// Fetch matches
 export async function getMatches() {
-  const matchesCol = collection(db, "matches");
-  const snapshot = await getDocs(matchesCol);
-  const matches = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  const querySnapshot = await getDocs(collection(db, "matches"));
+  const matches = [];
+
+  querySnapshot.forEach((doc) => {
+    matches.push({
+      id: doc.id,
+      ...doc.data()
+    });
+  });
+
   return matches;
 }
